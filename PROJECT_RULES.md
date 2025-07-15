5d66397e830f4e927b3d4f8858c86375dd20badc
CycleWorks <official.cycleworks@gmail.com>
2025-07-15 17:19:15 -0400

    Moved project rules to its own file

---
+++ PROJECT_RULES.md
@@ -0,0 +1,5 @@
+## Important rules to follow when developping CycleProject
+
+- No pointer shall be null (unless uninitialized)
+- Always handle edge-cases
+- Code should be written in a way that allows for easy future updates
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,6 @@
 #include "write.hpp"
 #include "errors.hpp"
 
-// Project rules:
-// 1: No pointer shall be null (unless uninitialized)
-// 2: Handle all edge cases, always assume that others will make stupid mistakes
-// 3: Think about growth when coding, making it easy to implement new features later on
-// 4: Never repeat the same code, use classes to make everything more complicated
-
 int main(int argc, char** argv){
     try {
     }
