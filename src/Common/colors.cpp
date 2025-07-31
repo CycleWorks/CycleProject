141d95fad1f22ab2d73c46ed6037c9e6481f65bf
CycleWorks <official.cycleworks@gmail.com>
2025-07-31 19:07:21 -0400

    - Made color initialization defined behavior - (WINDOWS ONLY) We now fetch the user's default console color on Windows

--- src/Common/colors.cpp
+++ src/Common/colors.cpp
@@ -1,30 +1,37 @@
 #include "Common/colors.hpp"
+#include <print>
 
-#ifdef OS_SUPPORTS_ANSI_ESCAPES
-    void Cycle::set_console_color(const OutputColor& foreground_color) noexcept {
-        std::print("{}", (const char*)foreground_color);
+uint _windows_default_color; // Set in _init_colors
+
+void _init_colors(){
+    #ifdef OS_WINDOWS
+        CONSOLE_SCREEN_BUFFER_INFO info;
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        if (GetConsoleScreenBufferInfo(hConsole, &info)) {
+            _windows_default_color = info.wAttributes & 0x0F;
+        }
+    #endif
+    set_console_color(Cycle::OutputColor::RESET);
+    std::atexit([](){
+        set_console_color(Cycle::OutputColor::RESET);
+    });
+}
+
+void Cycle::set_console_color(const OutputColor& color) noexcept {
+    static bool initialized = false;
+    if (!initialized){
+        initialized = true;
+        _init_colors();
     }
-#elifdef OS_WINDOWS
-    void Cycle::set_console_color(const OutputColor& color) noexcept {
+    #ifdef OS_SUPPORTS_ANSI_ESCAPES
+        std::print("{}", (const char*)foreground_color);
+    #elifdef OS_WINDOWS
         if (color == OutputColor::RESET){
-            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (unsigned int)OutputColor::WHITE);
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), _windows_default_color);
             return;
         }
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (unsigned int)color);
-    }
-#else
-    void Cycle::set_console_color(const OutputColor& foreground_color) noexcept {
-        return; // Do nothing
-    }
-#endif
-
-// Create and initialize colors
-struct InitColors {
-    InitColors(){
-        set_console_color(Cycle::OutputColor::RESET);
-        std::atexit([](){
-            set_console_color(Cycle::OutputColor::RESET);
-        });
-    }
-};
-InitColors init;
+    #else
+        return; // Do nothing, colors not supported
+    #endif
+}
